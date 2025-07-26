6e6d22840fcd7f1b01d7cd490e35b9e46911ce2f
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-07-26 20:03:40 -0300

    add

--- Aulas/Aula2/index.js
+++ Aulas/Aula2/index.js
@@ -23,15 +23,42 @@ let a = 5, b = 4, c = 10
 const _name = 'Wender'
 const $idade = 30
 
-const handleClick = () => {
-    alert(`${nome} você tem ${idade} anos`)
-    const cidade = prompt('Qual a sua cidade ?')
-    alert(`Sua cidade é ${cidade}`)
+const handleClick = (ev) => {
+
+    if (ev.className !== "Vermelho" && ev.className !== "Verde") {
+        alert(`${nome} você tem ${idade} anos`)
+        const cidade = prompt('Qual a sua cidade ?')
+        alert(`Sua cidade é ${cidade}`)
+    } else {
+        if (ev.className === 'Verde') {
+            alert('Você escolheu Verde')
+        } else {
+            alert('Você escolheu Vermelho')
+        }
+    }
 
 }
 
+const recursao = (n) => {
+    console.log(n)
+    if (n === 1) {
+        return 0
+    } else {
+        
+        
+        return recursao(n - 1)
+        
+    }
 
-console.log(Math.max(5,2,1,10)) // Retorna o número maior
+}
+
+console.log(recursao(20))
+
+
+
+console.log(Math.max(5, 2, 1, 10)) // Retorna o número maior
 console.log(Math.floor(5.14)) // Arredonda o número para inteiro menor
 
 console.log(Math.ceil(5.14)) // Arredonda para inteiro maior
+
+
