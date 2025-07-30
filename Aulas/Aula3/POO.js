d59869f6eef76fb31055f6880974dd9b00b4bd2a
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-07-30 18:51:18 -0300

    add

--- Aulas/Aula3/POO.js
+++ Aulas/Aula3/POO.js
@@ -4,7 +4,50 @@ const animal = {
         console.log('Au Au')
     },
 
+
 }
 
 
 console.log(animal)
+animal.latir()
+
+
+const pessoa = {
+    nome: 'Amparo',
+    getNome: function() {
+        return this.nome;
+    },
+
+    setNome: function(novo) {
+        this.nome = novo
+    },
+}
+
+console.log(pessoa.getNome())
+pessoa.setNome('Zé Naldo')
+console.log(pessoa.getNome())
+
+
+//prototype
+
+const text = 'test'
+
+console.log(Object.getPrototypeOf(text))
+
+const bool = true
+
+console.log(Object.getPrototypeOf(bool))
+
+const myObj = {
+    a: 'b'
+}
+
+console.log(Object.getPrototypeOf(myObj))
+
+const mySecondObj = Object.create(myObj)
+
+console.log(mySecondObj)
+
+//classes básicas
+
+
