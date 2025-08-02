0aec6cc7454812c31b5c0527db8cf390cdc896ae
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-08-02 17:22:26 -0300

    add

--- Aulas/Aula3/POO.js
+++ Aulas/Aula3/POO.js
@@ -1,6 +1,6 @@
 const animal = {
     nome: 'Bob',
-    latir: function() {
+    latir: function () {
         console.log('Au Au')
     },
 
@@ -14,11 +14,11 @@ animal.latir()
 
 const pessoa = {
     nome: 'Amparo',
-    getNome: function() {
+    getNome: function () {
         return this.nome;
     },
 
-    setNome: function(novo) {
+    setNome: function (novo) {
         this.nome = novo
     },
 }
@@ -51,3 +51,79 @@ console.log(mySecondObj)
 //classes básicas
 
 
+const cachorro = {
+    raca: null,
+    patas: 4,
+
+}
+
+const pastorAlemao = Object.create(cachorro)
+
+console.log(cachorro)
+
+
+pastorAlemao.raca = 'Pastor Alemão'
+
+console.log(pastorAlemao)
+console.log(pastorAlemao.patas)
+
+// Função construtora 
+
+
+function criarCachorro(nome, raca = 'Não definida') {
+    const cachorro = Object.create({})
+    cachorro.nome = nome
+    cachorro.raca = raca
+
+    return cachorro
+}
+
+
+const bob = criarCachorro('Bob', 'Vira Lata')
+const roby = criarCachorro('Roby')
+
+console.log(bob)
+console.log(roby)
+
+// Classes baseadas em funções   =>  new
+
+class Cachorro {
+    constructor(nome, raca = 'Não definida') {
+        this.nome = nome
+        this.raca = raca
+    }
+
+    mostra() {
+        console.log(`Nome: ${this.nome}  Raça: ${this.raca}`)
+    }
+}
+
+const husky = new Cachorro('Rabito')
+
+Cachorro.prototype.latir = function () {
+    console.log('Au au au auu')
+}
+
+husky.mostra()
+husky.latir()
+
+
+
+class Pet {
+    constructor(tipo, nome, raca = 'Não definida') {
+        this.tipo = tipo
+        this.nome = nome
+        this.raca = raca
+    }
+
+    cgar() {
+        console.log('Cagar')
+    }
+
+
+}
+
+const jeff = new Pet('Gato', 'Jeff')
+
+console.log(jeff)
+jeff.cgar()
