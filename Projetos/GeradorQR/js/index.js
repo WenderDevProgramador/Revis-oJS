b017089e36149b87fb55c3569fe9e3cdc6039a65
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-08-10 11:43:59 -0300

    add

--- Projetos/GeradorQR/js/index.js
+++ Projetos/GeradorQR/js/index.js
@@ -1,3 +1,55 @@
 // Variaveis 
 
-const container =  document.querySelector()
+const container = document.querySelector('.container')
+const qrCodeForm = document.querySelector('#qr-form button')
+
+const qrCodeInput = document.querySelector('#qr-form input')
+
+
+const qrCodeImg = document.querySelector('#qr-code img')
+
+// Funções 
+
+function generateQrCode() {
+
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if (!qrCodeInputValue) return;
+
+    qrCodeForm.innerText = 'Gerando código...'
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`
+
+
+
+    qrCodeImg.addEventListener('load', () => {
+        container.classList.add('active');
+    })
+
+    qrCodeForm.innerText = 'QR Gerado!'
+    qrCodeInput.value = ''
+
+
+}
+
+// Eventos
+
+
+qrCodeForm.addEventListener('click', () => {
+    generateQrCode()
+
+})
+
+qrCodeInput.addEventListener('keydown', (e) => {
+    if (e.code === 'Enter') {
+        generateQrCode()
+    }
+
+})
+
+qrCodeInput.addEventListener('click', (e) => {
+    container.classList.remove('active');
+    qrCodeForm.innerText = 'Gerar QR CODE'
+
+})
+
