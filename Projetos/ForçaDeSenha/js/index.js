7f0be67f79e666c1b228573dbcacd5f64cc3b6bb
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-08-16 18:05:51 -0300

    Add

--- Projetos/ForçaDeSenha/js/index.js
+++ Projetos/ForçaDeSenha/js/index.js
@@ -12,20 +12,52 @@ passwordInput.addEventListener('input', (e) => {
         2: 'Moderada',
         3: 'Forte',
         4: 'Muito forte',
+        5: 'Ideal'
     };
 
     let score = 0
 
     if (password.length >= 8) score++;
+    if (password.match(/[a-z]/)) score++;
+    if (password.match(/[A-Z]/)) score++;
+    if (password.match(/[0-9]/)) score++;
+    if (password.match(/[^a-zA-Z0-9]/)) score++;
 
-    const width = (score / 4) * 100;
+
+    switch (score) {
+
+        case 1:
+            strengthIndicator.style.backgroundColor = '#e70b0b'
+            break
+
+        case 2:
+            strengthIndicator.style.backgroundColor = '#ffb74d'
+            break
+
+        case 3:
+            strengthIndicator.style.backgroundColor = '#fff176'
+            break
+
+        case 4:
+            strengthIndicator.style.backgroundColor = '#81c796ff'
+            break
+
+        case 5:
+            strengthIndicator.style.backgroundColor = 'blue'
+            break
+
+        default:
+            strengthIndicator.style.backgroundColor = 'transparent'
+    }
+
+    const width = (score / 5) * 100;
 
     strengthIndicator.style.width = `${width}%`
 
     if (password.length > 0) {
-        strenghText.innerHTML = `Força ${streghs[score]}`
+        strenghText.innerHTML = `Força:  ${streghs[score]}`
     } else {
         strenghText.innerHTML = '';
-        
+
     }
 })
