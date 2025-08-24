55ec9b82cf6ab460823c691e521c52bf91e3a8e0
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-08-24 11:49:42 -0300

    add

---
+++ Aulas/Aula11-Debugs e tratamento/index.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const banana = 'Fruta'
+
+// delete Object.prototype;
+
+function teste() {
+    'use strict'
+    const testando = 'teste'
+}
+
+teste()
+
+
+let p = {a: 2, b: 1}
+
+
+
+
+
