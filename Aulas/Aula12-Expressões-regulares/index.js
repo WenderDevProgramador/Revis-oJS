6bc250dcd05aa482c6c85b1141e77c9735db708e
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-08-25 19:12:49 -0300

    add

--- Aulas/Aula12-Expressões-regulares/index.js
+++ Aulas/Aula12-Expressões-regulares/index.js
@@ -2,11 +2,28 @@
 
 
 const input = document.querySelector('#regex')
+const main = document.querySelector('main')
+const div = document.createElement('div')
+div.classList.add('dados')
+div.style.display = 'none'
+
 
 input.addEventListener('input', (e) => {
     "use strict"
+    div.style.display = 'block'
+    
     let valor = e.target.value
+
+    if (!valor || valor === '' ) {
+        div.style.display = 'none'
+    }
+    
     const n = valor.replace(/\D/g, '')
-    console.log(n)
+
+    div.innerText = n
+
+    main.appendChild(div)
+
+
 })
 
