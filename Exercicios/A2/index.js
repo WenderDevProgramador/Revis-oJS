293fbc8a2b913b7697a4bd64a056235add7b2b74
WENDER R DE QUEIROZ <166562387+WenderDevProgramador@users.noreply.github.com>
2025-09-04 19:09:54 -0300

    add

---
+++ Exercicios/A2/index.js
@@ -0,0 +1,12 @@
+
+const inputText = document.querySelector('#cesar')
+const button = document.querySelector('#converter')
+const resultado = document.querySelector('#resultado')
+const numinput = document.querySelector('#num')
+
+const arrayAlfa = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
